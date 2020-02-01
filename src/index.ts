b2124ca3f3cc23ee019d13801445a1429f19e829
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2020-02-01 08:01:34 -0500

    filter for inbound only

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,36 @@ import twilio from 'twilio';
 import papaparse from 'papaparse';
 import fs from 'fs';
 
-const NOW = new Date();
+// const NOW = new Date();
 
 async function main(): Promise<void> {
   const instance = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
-  const searchAfter = new Date('2019-09-06 11:59:43.651717+00');
+  const searchAfter = new Date('2020-01-31 20:00:00+00');
+  const searchBefore = new Date('2020-02-01 00:30:00+00');
 
   const messages = await instance.messages.list({
     dateSentAfter: searchAfter,
-    dateSentBefore: NOW
+    dateSentBefore: searchBefore
   });
 
-  const rows = messages.map(m => {
-    return {
-      contact_number: m.direction === 'inbound' ? m.from : m.to,
-      created_at: m.dateCreated,
-      is_from_contact: m.direction === 'inbound',
-      num_media: m.numMedia,
-      num_segments: m.numSegments,
-      send_status: m.status,
-      sent_at: m.dateSent,
-      service_id: m.direction === 'inbound' ? m.messagingServiceSid : m.sid,
-      service_response_at: m.dateUpdated,
-      text: m.body,
-      updated_at: m.dateUpdated,
-      user_id: process.env.HARDCODED_USER_ID
-    };
-  });
+  const rows = messages
+    .filter(m => m.direction === 'inbound')
+    .map(m => {
+      return {
+        contact_number: m.direction === 'inbound' ? m.from : m.to,
+        created_at: m.dateCreated,
+        is_from_contact: m.direction === 'inbound',
+        num_media: m.numMedia,
+        num_segments: m.numSegments,
+        send_status: m.status,
+        sent_at: m.dateSent,
+        service_id: m.direction === 'inbound' ? m.messagingServiceSid : m.sid,
+        service_response_at: m.dateUpdated,
+        text: m.body,
+        updated_at: m.dateUpdated
+      };
+    });
 
   const output = papaparse.unparse(rows);
   fs.writeFileSync('./messages.csv', output);
